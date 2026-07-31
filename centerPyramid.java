66d6533c5c6aa72388a06438d26e7d29ee9f80c8
Muskan gupta <muskangupta2118@gmail.com>
2026-07-31 23:13:41 +0530

    center pyramid

--- centerPyramid.java
+++ centerPyramid.java
@@ -1,6 +1,6 @@
 public class Pyramid{
   public static void main (String[] args){
-    int n=5;
+    int n=10;
     for (int i=1; i<=n;i++){
         for(int j=1;j<=n-i;j++){
             System.out.print(" ");
